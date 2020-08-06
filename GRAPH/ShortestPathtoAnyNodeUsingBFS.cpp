f2d8939d89f7b83ddbfb8b5d51a95ee15220da13
Navdeep Singh <navdeep.singh@hashedin.com>
2020-08-06 10:44:27 +0530

    Shortest Path TO any Node using BFS

---
+++ GRAPH/ShortestPathtoAnyNodeUsingBFS.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include<list>
+#include<queue>
+using namespace std;
+
+
+class Graph{
+    int V;
+    list<int>*adjList;
+    public:
+    Graph(int v){
+        V=v;
+        adjList= new list<int>[V];
+    }
+    void addEdge(int u,int v ,bool bidir=true)
+    {
+        adjList[u].push_back(v);
+        if(bidir)
+            adjList[v].push_back(u);
+    }
+    void printadjList(){
+        for(int i=0;i<V;i++)
+        {
+            cout<<i<<"->";
+            for(auto node:adjList[i])
+            {
+                cout<<node<<" ,";
+            }
+            cout<<endl;
+        }
+    }
+    
+    void bfs(int src,int destination)
+    {
+        queue<int>q;
+        bool *visited = new bool[V+1]{0};
+        int *distance = new int[V+1]{0};
+        q.push(src);
+        visited[src]=true;
+        while(!q.empty())
+        {
+            int node=q.front();
+            cout<<node<<" ";
+            q.pop();
+            for(auto neighbour:adjList[node])
+            {
+                if(!visited[neighbour])
+                {
+                    q.push(neighbour);
+                    visited[neighbour]=true;
+                    distance[neighbour]=distance[node]+1;
+                }
+            }
+        }
+        cout<<endl;
+        for(int i=0;i<V;i++)
+        {
+            cout<<i<<" node has distance "<<distance[i]<<endl;
+        }
+    }
+};
+
+int main()
+{
+    Graph g(6);
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(0,4);
+    g.addEdge(2,4);
+    g.addEdge(3,2);
+    g.addEdge(2,3);
+    g.addEdge(3,5);
+    g.addEdge(3,4);
+    // g.printadjList();
+    g.bfs(0,3);
+    return 0;
+}
